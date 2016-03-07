7f3c6dff1bf96def1e89e64af27e196c8593927d
Jonybang <jonybang@mail.ru>
2016-03-07 22:43:43 +1000

    fix layout

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -1,9 +1,15 @@
-{!! Form::open(array('url' => '/', 'files' => true)) !!}
-    <div>{!! Form::label('price', 'Цена') !!}</div>
-    <div>{!! Form::text('price', '', ['class'=>'form-control', 'required']) !!}</div>
-    <div>{!! Form::label('text', 'Пожелания') !!}</div>
-    <div>{!! Form::textarea('text', '', ['class'=>'form-control', 'rows' => 10]) !!}</div>
-    <div>{!! Form::submit('Купить', ['class'=>'btn btn-primary']) !!}</div>
-{!! Form::close() !!}
-<br>
-Исходники: <a href="https://github.com/Jonybang/AdvCash">https://github.com/Jonybang/AdvCash</a>
+@extends('layout')
+
+@section('content')
+
+    {!! Form::open(array('url' => '/', 'files' => true)) !!}
+        <div>{!! Form::label('price', 'Цена') !!}</div>
+        <div>{!! Form::text('price', '', ['class'=>'form-control', 'required']) !!}</div>
+        <div>{!! Form::label('text', 'Пожелания') !!}</div>
+        <div>{!! Form::textarea('text', '', ['class'=>'form-control', 'rows' => 10]) !!}</div>
+        <div>{!! Form::submit('Купить', ['class'=>'btn btn-primary']) !!}</div>
+    {!! Form::close() !!}
+
+    <br>
+    Исходники: <a href="https://github.com/Jonybang/AdvCash">https://github.com/Jonybang/AdvCash</a>
+@endsection
